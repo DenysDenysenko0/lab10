8f4c4677b3f7a2bc90794f58f468c4abe8e34282
AlexKubusha <kubusha.oleksii@student.sumdu.edu.ua>
2025-04-28 20:12:40 +0300

    Додано код Кубуші та Оновлення файлу

--- 1.py
+++ 1.py
@@ -18,15 +18,32 @@ def Open(file_name, mode):
 filename = "file.txt"
 
 # Код Денисенка Дениса
-file1 = Open(filename, "w")
+file1 = Open(filename, "w") # "w" - відкриває файл для запису, перезаписуючи існуючий вміст.
 
 if file1:
     # Введення даних
-    pryzvishe = input("Введіть прізвище: ")
+    pryzvishe = input("Введіть прізвище (№1): ")
     pytanya = input("Введіть питання: ")
 
     # Запис даних
     file1.write(f"{pryzvishe}\n")
+    file1.write(f"{pytanya}\n\n")
+
+    print("Текстовий файл успішно змінено")
+    file1.close()
+
+# Код Кубуші Олексія
+file1 = Open(filename, "a") # "a" - відкриває файл для додавання нових даних в кінець файлу, не змінюючи існуючий вміст.
+
+if file1:
+    # Введення даних
+    pryzvishe = input("Введіть прізвище (№2): ")
+    vidpovid = input("Введіть відповідь на питання: ")
+    pytanya = input("Введіть питання: ")
+
+    # Запис даних
+    file1.write(f"{pryzvishe}\n")
+    file1.write(f"{vidpovid}\n")
     file1.write(f"{pytanya}\n\n")
 
     print("Текстовий файл успішно змінено")
